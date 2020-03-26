53e8cfc7fea47159d6b2071ade7fa90488b798fe
ZenekeZene <hectorvillarm@gmail.com>
2020-03-27 00:21:09 +0100

    Favicon. Lightness and saturation functions

--- src/components/documentation/components/Buttons.vue
+++ src/components/documentation/components/Buttons.vue
@@ -7,10 +7,15 @@
       <button color-secondary>Secundario</button>
       <button color-tertiary>Terciario</button>
       <br/>
-      <button outlined>Terciario</button>
-      <button outlined color-primary>Terciario</button>
-      <button outlined color-secondary>Terciario</button>
-      <button outlined color-tertiary>Terciario</button>
+      <button outlined>Outlined</button>
+      <button outlined color-primary>Outlined</button>
+      <button outlined color-secondary>Outlined</button>
+      <button outlined color-tertiary>Outlined</button>
+      <br/>
+      <button hover>Hover</button>
+      <button hover color-primary>Hover</button>
+      <button hover color-secondary>Hover</button>
+      <button hover color-tertiary>Hover</button>
       <br/>
       <button transparent>Transparente</button>
       <button transparent color-primary>Transparente</button>
