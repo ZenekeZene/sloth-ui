a20e5475e8b80e85c9befbf5ef5e2dfcf7280d8b
ZenekeZene <hectorvillarm@gmail.com>
2020-09-30 11:37:29 +0200

    Storybook added

---
+++ .storybook/preview.js
@@ -0,0 +1,5 @@
+import '../lib/styles/main.scss';
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+}
---
+++ src/stories/2-Callout.stories.js
@@ -0,0 +1,7 @@
+import '../../lib/styles/main.scss';
+
+export default {
+  title: 'Callout',
+};
+
+export const Callout = () => '<div class="button"><p>Important information</p></div>';
